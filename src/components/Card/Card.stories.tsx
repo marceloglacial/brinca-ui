ccecd3d6f7c2c0ea73e36309d1e9a607bf9bb839
marceloglacial <glacial@gmail.com>
2022-03-28 13:40:47 -0400

    add ned stories and footer

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -19,23 +19,39 @@ const Template: Story<any> = (args) => (
         <Card {...args} />
     </div>
 )
-const imageProps: CardImageProps = {
-    src: 'https://www.brinca.ca/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdw2wjwhuv%2Fimages%2Ff_auto%2Cq_auto%2Fv1634082453%2Fbrinca%2Feventos%2F2021-picnic%2FBRINCA_-_PICNIC-65_dru6mn%2FBRINCA_-_PICNIC-65_dru6mn.jpg%3F_i%3DAA&w=3840&q=75',
+
+const data = {
+    image: {
+        src: 'https://www.brinca.ca/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Fdw2wjwhuv%2Fimages%2Ff_auto%2Cq_auto%2Fv1634082453%2Fbrinca%2Feventos%2F2021-picnic%2FBRINCA_-_PICNIC-65_dru6mn%2FBRINCA_-_PICNIC-65_dru6mn.jpg%3F_i%3DAA&w=3840&q=75',
+    },
+    header: 'Card Header',
+    content: `Et Lorem commodo ex fugiat aliquip pariatur qui mollit
+nostrud velit magna eu. Dolore consectetur sit ut irure nisi
+ipsum anim aute. Enim quis excepteur consequat commodo
+incididunt dolor excepteur.`,
+    button: 'Button',
 }
 
 export const Base = Template.bind({})
 Base.args = {
     children: (
         <div>
-            <CardImage {...imageProps} />
             <CardBody>
-                <CardHeader>Card Header</CardHeader>
-                <CardContent>
-                    Et Lorem commodo ex fugiat aliquip pariatur qui mollit
-                    nostrud velit magna eu. Dolore consectetur sit ut irure nisi
-                    ipsum anim aute. Enim quis excepteur consequat commodo
-                    incididunt dolor excepteur.
-                </CardContent>
+                <CardHeader>{data.header}</CardHeader>
+                <CardContent>{data.content}</CardContent>
+            </CardBody>
+        </div>
+    ),
+}
+
+export const WithImage = Template.bind({})
+WithImage.args = {
+    children: (
+        <div>
+            <CardImage {...data.image} />
+            <CardBody>
+                <CardHeader>{data.header}</CardHeader>
+                <CardContent>{data.content}</CardContent>
             </CardBody>
         </div>
     ),
@@ -45,20 +61,15 @@ export const WithButton = Template.bind({})
 WithButton.args = {
     children: (
         <div>
-            <CardImage {...imageProps} />
+            <CardImage {...data.image} />
             <CardBody>
-                <CardHeader>Card Header</CardHeader>
-                <CardContent>
-                    Et Lorem commodo ex fugiat aliquip pariatur qui mollit
-                    nostrud velit magna eu. Dolore consectetur sit ut irure nisi
-                    ipsum anim aute. Enim quis excepteur consequat commodo
-                    incididunt dolor excepteur.
-                    <CardFooter>
-                        <Button variant='secondary' full>
-                            Button
-                        </Button>
-                    </CardFooter>
-                </CardContent>
+                <CardHeader>{data.header}</CardHeader>
+                <CardContent>{data.content}</CardContent>
+                <CardFooter>
+                    <Button variant='primary' full>
+                        {data.button}
+                    </Button>
+                </CardFooter>
             </CardBody>
         </div>
     ),
