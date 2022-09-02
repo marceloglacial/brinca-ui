80f86ddb688fd186c69a9f07df3b33c57fe81558
Marcelo <glacial@gmail.com>
2022-09-02 08:41:19 -0400

    add car stories

--- src/components/BusinessCard/BusinessCardStyles.tsx
+++ src/components/BusinessCard/BusinessCardStyles.tsx
@@ -1,10 +1,10 @@
 const styles = {
     container:
-        'brinca-ui-business-card shadow-2xl p-4 text-sm flex flex-col gap-2 items-center lg:items-start',
-    header: 'brinca-ui-business-card__header flex flex-col lg:flex-row gap-4 items-center',
-    content: 'brinca-ui-business-card__content',
-    footer: 'brinca-ui-business-card__footer text-sm',
-    image: 'brinca-ui-business-card__image max-w-[150px]',
-    info: 'brinca-ui-business-card__info',
+        'brinca-ui-business-card shadow-2xl p-6 text-sm flex flex-col gap-4 bg-white',
+    header: 'brinca-ui-business-card__header text-center lg:text-left grid lg:grid-cols-[150px_1fr] gap-4',
+    content: 'brinca-ui-business-card__info',
+    info: 'brinca-ui-business-card__content text-center lg:text-left  flex flex-col',
+    footer: 'brinca-ui-business-card__footer text-sm text-center flex flex-wrap justify-center lg:justify-start gap-2',
+    image: 'brinca-ui-business-card__image overflow-hidden',
 }
 export default styles
