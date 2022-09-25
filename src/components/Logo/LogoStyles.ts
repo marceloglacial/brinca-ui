c9765516ebbcca0fdb625b832c702a1128c98b2f
Marcelo <glacial@gmail.com>
2022-09-24 20:04:30 -0400

    add sizes

--- src/components/Logo/LogoStyles.ts
+++ src/components/Logo/LogoStyles.ts
@@ -1,5 +1,10 @@
 const styles = {
-    container: `logo-container w-[150px] lg:w-[230px]`,
+    container: `logo-container`,
     image: `logo-image w-full h-auto`,
+    sizes: {
+        xs: 'w-[150px]',
+        md: 'w-[150px] lg:w-[230px]',
+        lg: 'w-[230px] lg:w-[460px]',
+    },
 }
 export default styles
