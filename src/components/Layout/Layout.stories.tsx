15609aaa204bff95ff230037b023e0ee69401646
Marcelo <glacial@gmail.com>
2022-12-14 00:10:23 -0500

    update layout

--- src/components/Layout/Layout.stories.tsx
+++ src/components/Layout/Layout.stories.tsx
@@ -46,82 +46,124 @@ Home.args = {
                 <Heading>Excepteur est enim</Heading>
                 <div className='grid gap-8 md:grid-cols-2 lg:grid-cols-3'>
                     <a href='#'>
-                        <Card
-                            title='Officia duis id veniam minim '
-                            description='Sint Lorem ipsum minim dolor elit excepteur est enim ad voluptate minim labore ipsum pariatur. Ex irure veniam aliqua eu id '
-                            image={
+                        <Card>
+                            <Card.Image>
                                 <img
                                     src={`https://images.unsplash.com/photo-1593995863951-57c27e518295?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGJyYXppbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60`}
                                     alt={`Card Image`}
                                     className='w-full h-full object-cover object-top'
                                 />
-                            }
-                        />
+                            </Card.Image>
+                            <Card.Body>
+                                <h4>Dolore sunt nostrud laboris sunt est in</h4>
+                                <p>
+                                    Est ea laborum laboris sunt et ut aliquip
+                                    occaecat et dolor. Excepteur nostrud laboris
+                                    nostrud cupidatat proident esse do ut qui
+                                    irure excepteur.
+                                </p>
+                            </Card.Body>
+                        </Card>
                     </a>
                     <a href='#'>
-                        <Card
-                            title='Officia duis id veniam minim '
-                            description='Sint Lorem ipsum minim dolor elit excepteur est enim ad voluptate minim labore ipsum pariatur. Ex irure veniam aliqua eu id '
-                            image={
+                        <Card>
+                            <Card.Image>
                                 <img
                                     src={`https://images.unsplash.com/photo-1543957568-897d11ea3926?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzZ8fGJyYXppbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60`}
                                     alt={`Card Image`}
                                     className='w-full h-full object-cover'
                                 />
-                            }
-                        />
+                            </Card.Image>
+                            <Card.Body>
+                                <h4>Dolore sunt nostrud laboris sunt est in</h4>
+                                <p>
+                                    Est ea laborum laboris sunt et ut aliquip
+                                    occaecat et dolor. Excepteur nostrud laboris
+                                    nostrud cupidatat proident esse do ut qui
+                                    irure excepteur.
+                                </p>
+                            </Card.Body>
+                        </Card>
                     </a>
                     <a href='#'>
-                        <Card
-                            title='Officia duis id veniam minim '
-                            description='Sint Lorem ipsum minim dolor elit excepteur est enim ad voluptate minim labore ipsum pariatur. Ex irure veniam aliqua eu id '
-                            image={
+                        <Card>
+                            <Card.Image>
                                 <img
                                     src={`https://images.unsplash.com/photo-1634484850417-b8f44ae25cb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGZvcnRhbGV6YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60`}
                                     alt={`Card Image`}
                                     className='w-full h-full object-cover'
                                 />
-                            }
-                        />
+                            </Card.Image>
+                            <Card.Body>
+                                <h4>Dolore sunt nostrud laboris sunt est in</h4>
+                                <p>
+                                    Est ea laborum laboris sunt et ut aliquip
+                                    occaecat et dolor. Excepteur nostrud laboris
+                                    nostrud cupidatat proident esse do ut qui
+                                    irure excepteur.
+                                </p>
+                            </Card.Body>
+                        </Card>
                     </a>
                     <a href='#'>
-                        <Card
-                            title='Officia duis id veniam minim '
-                            description='Sint Lorem ipsum minim dolor elit excepteur est enim ad voluptate minim labore ipsum pariatur. Ex irure veniam aliqua eu id '
-                            image={
+                        <Card>
+                            <Card.Image>
                                 <img
                                     src={`https://images.unsplash.com/photo-1552035264-b9ad6e681ca7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODR8fGJyYXppbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60`}
                                     alt={`Card Image`}
                                     className='w-full h-full object-cover'
                                 />
-                            }
-                        />
+                            </Card.Image>
+                            <Card.Body>
+                                <h4>Dolore sunt nostrud laboris sunt est in</h4>
+                                <p>
+                                    Est ea laborum laboris sunt et ut aliquip
+                                    occaecat et dolor. Excepteur nostrud laboris
+                                    nostrud cupidatat proident esse do ut qui
+                                    irure excepteur.
+                                </p>
+                            </Card.Body>
+                        </Card>
                     </a>
                     <a href='#'>
-                        <Card
-                            title='Officia duis id veniam minim '
-                            description='Sint Lorem ipsum minim dolor elit excepteur est enim ad voluptate minim labore ipsum pariatur. Ex irure veniam aliqua eu id '
-                            image={
+                        <Card>
+                            <Card.Image>
                                 <img
                                     src={`https://images.unsplash.com/photo-1566925068640-79b2cdba4491?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fGJyYXppbHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60`}
                                     alt={`Card Image`}
                                     className='w-full h-full object-cover'
                                 />
-                            }
-                        />
+                            </Card.Image>
+                            <Card.Body>
+                                <h4>Dolore sunt nostrud laboris sunt est in</h4>
+                                <p>
+                                    Est ea laborum laboris sunt et ut aliquip
+                                    occaecat et dolor. Excepteur nostrud laboris
+                                    nostrud cupidatat proident esse do ut qui
+                                    irure excepteur.
+                                </p>
+                            </Card.Body>
+                        </Card>
                     </a>
                     <a href='#'>
-                        <Card
-                            title='Officia duis id veniam minim '
-                            description='Sint Lorem ipsum minim dolor elit excepteur est enim ad voluptate minim labore ipsum pariatur. Ex irure veniam aliqua eu id '
-                            image={
+                        <Card>
+                            <Card.Image>
                                 <img
                                     src={`https://images.unsplash.com/photo-1550574364-d19c7a455f91?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80`}
                                     alt={`Card Image`}
                                     className='w-full h-full object-cover'
                                 />
-                            }
-                        />
+                            </Card.Image>
+                            <Card.Body>
+                                <h4>Dolore sunt nostrud laboris sunt est in</h4>
+                                <p>
+                                    Est ea laborum laboris sunt et ut aliquip
+                                    occaecat et dolor. Excepteur nostrud laboris
+                                    nostrud cupidatat proident esse do ut qui
+                                    irure excepteur.
+                                </p>
+                            </Card.Body>
+                        </Card>
                     </a>
                 </div>
                 <div className='text-center'>
