a9c2b1262e5bd59c83304eaec50fd3b21cfb23f3
Marcelo <glacial@gmail.com>
2022-12-20 14:36:58 -0500

    fix links and navbar stories

--- src/components/NavBar/NavBar.stories.tsx
+++ src/components/NavBar/NavBar.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavBar } from './NavBar'
-import Link from '../Link/Link'
+import { Link } from '../Link/Link'
 
 export default {
     title: 'Components/NavBar',
