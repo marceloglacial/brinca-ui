b9e03214a5c75408a8308ad92077d279c1471dbf
Marcelo <glacial@gmail.com>
2022-12-21 16:08:58 -0500

    add section tests

--- src/components/Section/SectionStyles.ts
+++ src/components/Section/SectionStyles.ts
@@ -1,8 +1,8 @@
 const styles = {
-    container: `main`,
-    s: `space-y-3 md:space-y-4`,
-    m: `space-y-4 md:space-y-8`,
-    l: `space-y-8 md:space-y-16`,
-    xl: `space-y-16 md:space-y-24`,
+    container: `section`,
+    s: `section--s space-y-3 md:space-y-4`,
+    m: `section--m space-y-4 md:space-y-8`,
+    l: `section--l space-y-8 md:space-y-16`,
+    xl: `section--xl space-y-16 md:space-y-24`,
 }
 export default styles
