385c0590e67b73eb137e90a7f6ccd7e9ac6b70e8
Marcelo <glacial@gmail.com>
2022-12-21 17:12:36 -0500

    test heading styles

--- src/components/Heading/HeadingStyles.ts
+++ src/components/Heading/HeadingStyles.ts
@@ -1,18 +1,4 @@
 const styles = {
-    container: `heading
-                pb-2
-                md:pb-4
-                text-center 
-                font-bold 
-                relative 
-                after:absolute 
-                after:left-[50%] 
-                after:translate-x-[-50%] 
-                after:bottom-0 
-                after:h-[2px] 
-                after:w-12
-                md:after:w-24
-                after:rounded-full 
-                after:bg-black`,
+    container: `heading pb-2 md:pb-4 text-center font-bold relative after:absolute after:left-[50%] after:translate-x-[-50%] after:bottom-0 after:h-[2px] after:w-12 md:after:w-24 after:rounded-full after:bg-black`,
 }
 export default styles
