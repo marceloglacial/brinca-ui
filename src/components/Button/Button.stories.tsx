ef7c319f2f56dabb273c4dc898de165136af4ba3
marceloglacial <glacial@gmail.com>
2024-07-26 16:43:33 -0400

    fix button sizes

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -29,3 +29,36 @@ FullWidth.args = {
     children: 'Button Full',
     full: true,
 }
+
+export const Sizes = () => {
+    return (
+        <div className='grid gap-4'>
+            <div className='flex items-center gap-4'>
+                <div>
+                    <Button size='sm'>Small</Button>
+                </div>
+                <div>
+                    <Button size='md'>Medium</Button>
+                </div>
+                <div>
+                    <Button size='lg'>Large</Button>
+                </div>
+            </div>
+            <div className='flex items-center gap-4'>
+                <div>
+                    <Button variant='secondary' size='sm'>
+                        Small
+                    </Button>
+                </div>
+                <div>
+                    <Button variant='secondary'>Medium</Button>
+                </div>
+                <div>
+                    <Button variant='secondary' size='lg'>
+                        Large
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
+}
