442340ecc5e035b8491292a3a3be849d57c8aec1
marceloglacial <glacial@gmail.com>
2024-08-09 18:59:54 -0400

    fix input disabled

--- src/components/Form/FormInput.tsx
+++ src/components/Form/FormInput.tsx
@@ -6,14 +6,11 @@ export interface FormInputProps {
 }
 
 const FormInput: FC<FormInputProps & JSX.IntrinsicElements['input']> = (props): JSX.Element => {
-    const { full, type = 'text', ...inputProps } = props
+    const { full, type = 'text', disabled, ...inputProps } = props
     const fullClassName = full ? styles.inputFull : ''
-    return (
-        <input
-            {...inputProps}
-            type={type}
-            className={`${styles[type] || styles.text} ${fullClassName} `}
-        />
-    )
+    const allStyles = disabled
+        ? styles.disabled
+        : `${styles[type] || styles.text} ${fullClassName} `
+    return <input {...inputProps} type={type} className={allStyles} />
 }
 export default FormInput
