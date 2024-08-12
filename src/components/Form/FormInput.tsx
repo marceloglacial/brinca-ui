6e614034d30632fa06103546ae681272908c579b
marceloglacial <glacial@gmail.com>
2024-08-12 14:57:40 -0400

    fix disabled and focus

--- src/components/Form/FormInput.tsx
+++ src/components/Form/FormInput.tsx
@@ -8,9 +8,14 @@ export interface FormInputProps {
 const FormInput: FC<FormInputProps & JSX.IntrinsicElements['input']> = (props): JSX.Element => {
     const { full, type = 'text', disabled, ...inputProps } = props
     const fullClassName = full ? styles.inputFull : ''
-    const allStyles = disabled
-        ? styles.disabled
-        : `${styles[type] || styles.text} ${fullClassName} `
-    return <input {...inputProps} type={type} className={allStyles} />
+    const disabledClassname = disabled ? styles.disabled : ` `
+    return (
+        <input
+            {...inputProps}
+            type={type}
+            className={`${styles[type] || styles.text} ${fullClassName} ${disabledClassname}`}
+            disabled={disabled}
+        />
+    )
 }
 export default FormInput
--- src/components/Form/FormStyles.ts
+++ src/components/Form/FormStyles.ts
@@ -8,19 +8,19 @@ type stylesType = {
 const formButtonClass = `form__button ${buttonClass.button} ${buttonClass.secondary} ${buttonClass.size.md} hover:cursor-pointer`
 
 const styles: stylesType = {
+    button: formButtonClass,
+    checkbox: `form__checkbox appearance-none relative hover:cursor-pointer after:block after:w-6 after:h-6 after:checked:bg-green-600 after:rounded-lg after:border-2 after:border-green-600 focus:ring-green-500 focus:ring-2 before:absolute before:top-[4px] before:left-[3px] before:z-10 before:checked:block before:w-4 before:h-4 before:checked:bg-[url('https://res.cloudinary.com/brinca/image/upload/v1671588190/brinca-ui/checkbox-white_dqjam0.svg')] before:bg-no-repeat before:bg-cover`,
+    disabled: `form__disabled text-gray-400 border-gray-300 bg-gray-50 hover:cursor-not-allowed hover:text-gray-400 hover:border-gray-300 hover:bg-gray-50 after:border-gray-400`,
     form: `form mx-auto max-w-2xl space-y-8`,
     group: `form__group flex flex-wrap gap-4 items-center`,
-    label: `form__label font-bold`,
-    text: `form__input rounded-2xl border-2 border-green-600 py-4 px-6`,
-    textarea: `form__textarea rounded-2xl border-2 border-green-600 py-4 px-6`,
     inputFull: `w-full`,
-    selectContainer: `form__select-container inline-block relative after:absolute after:top-4 after:right-3 after:flex after:items-center after:justify-center after:leading-none after:w-4 after:h-4 after:text-xl after:text-green-600 after:content-['⌄'] after:z-0`,
-    select: `form__select appearance-none relative rounded-2xl border-2 border-green-600 py-4 pl-4 pr-10 bg-transparent z-10`,
-    button: formButtonClass,
-    submit: formButtonClass,
-    disabled: `form__button ${buttonClass.button} ${buttonClass.disabled} ${buttonClass.size.md}`,
-    checkbox: `form__checkbox appearance-none  relative hover:cursor-pointer after:block after:w-6 after:h-6 after:checked:bg-green-600 after:rounded-lg after:border-2 after:border-green-600 focus:ring-green-500 focus:ring-2 before:absolute before:top-[4px] before:left-[3px] before:z-10 before:checked:block before:w-4 before:h-4 before:checked:bg-[url('https://res.cloudinary.com/brinca/image/upload/v1671588190/brinca-ui/checkbox-white_dqjam0.svg')] before:bg-no-repeat before:bg-cover`,
+    label: `form__label font-bold`,
     radio: `form__radio appearance-none  relative hover:cursor-pointer after:block after:w-6 after:h-6 after:checked:bg-green-600 after:rounded-full after:border-2 after:border-green-600 focus:ring-green-500 focus:ring-2 before:absolute before:top-[7px] before:left-[7px] before:z-10 before:rounded-full before:checked:block before:w-[10px] before:h-[10px] before:bg-white`,
+    select: `form__select appearance-none relative rounded-2xl border-2 border-green-600 py-4 pl-4 pr-10 bg-transparent z-10`,
+    selectContainer: `form__select-container inline-block relative after:absolute after:top-4 after:right-3 after:flex after:items-center after:justify-center after:leading-none after:w-4 after:h-4 after:text-xl after:text-green-600 after:content-['⌄'] after:z-0`,
+    submit: formButtonClass,
+    text: `form__input rounded-2xl border-2 border-green-600 py-4 px-6 focus-visible:outline-green-600 focus-visible:outline-offset-2 focus-visible:outline-2`,
+    textarea: `form__textarea rounded-2xl border-2 border-green-600 py-4 px-6`,
 }
 
 export default styles
