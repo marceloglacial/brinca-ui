8d23e9e0e2bdb1f11f4a84ac634c25b4a1455785
marceloglacial <glacial@gmail.com>
2024-08-12 15:49:55 -0400

    fix focus and disable

--- src/components/Form/FormInput.tsx
+++ src/components/Form/FormInput.tsx
@@ -8,12 +8,12 @@ export interface FormInputProps {
 const FormInput: FC<FormInputProps & JSX.IntrinsicElements['input']> = (props): JSX.Element => {
     const { full, type = 'text', disabled, ...inputProps } = props
     const fullClassName = full ? styles.inputFull : ''
-    const disabledClassname = disabled ? styles.disabled : ` `
+    const disabledClassName = disabled ? styles.disabled : ''
     return (
         <input
             {...inputProps}
             type={type}
-            className={`${styles[type] || styles.text} ${fullClassName} ${disabledClassname}`}
+            className={`${styles[type] || styles.text} ${fullClassName} ${disabledClassName}`}
             disabled={disabled}
         />
     )
